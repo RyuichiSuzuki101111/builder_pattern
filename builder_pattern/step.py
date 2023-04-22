2f94cf07a0aaff0618ba5ef20f01e3a455ce05fd
Ryuichi Suzuki <untitled.tex@gmail.com>
2023-04-22 13:29:15 +0900

    Add class docstrings
    Add docstrings for the _step, build_step, and process_step classes

---
+++ builder_pattern/step.py
@@ -0,0 +1,6 @@
+"""
+step.py
+
+This module provides the base step class and its derivatives, build_step and process_step.
+These classes serve as a foundation for defining a series of steps in a builder pattern.
+"""
